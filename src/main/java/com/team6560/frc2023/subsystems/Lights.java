6bf381d96696f5f21cd67ad0b5125adaf6db9600
Team Laptop <first.robotics.portola@gmail.com>
2023-03-07 18:34:48 -0800

    changes

---
+++ src/main/java/com/team6560/frc2023/subsystems/Lights.java
@@ -0,0 +1,55 @@
+package com.team6560.frc2023.subsystems;
+
+import org.w3c.dom.css.RGBColor;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Lights extends SubsystemBase {
+    private AddressableLED led;
+    private AddressableLEDBuffer ledbuffer;
+
+    public Lights() {
+        led = new AddressableLED(0);
+        ledbuffer = new AddressableLEDBuffer(6);
+        led.setLength(ledbuffer.getLength());
+        setLightsToDefault();
+    }
+
+    public void setLightsToDefault() {
+        ledbuffer.setRGB(0, 41, 202, 254 );
+        ledbuffer.setRGB(1, 255, 255, 255 );
+        ledbuffer.setRGB(2, 41, 202, 254 );
+        ledbuffer.setRGB(3, 255, 255, 255 );
+        ledbuffer.setRGB(4, 41, 202, 254 );
+        ledbuffer.setRGB(5, 255, 255, 255 );
+
+        led.setData(ledbuffer);
+        led.start();
+    }
+
+    public void setLightsToCone() {
+        for( int i = 0; i < ledbuffer.getLength(); i++ ) {
+            ledbuffer.setRGB(i, 255, 255, 0);
+        }
+        led.setData(ledbuffer);
+        led.start();
+    }
+
+    public void setLightsToCube() {
+        for( int i = 0; i < ledbuffer.getLength(); i++ ) {
+            ledbuffer.setRGB(i, 128, 0, 128);
+        }
+        led.setData(ledbuffer);
+        led.start();
+    }
+
+    public void setLights(int red, int green, int blue) {
+        for( int i = 0; i < ledbuffer.getLength(); i++ ) {
+            ledbuffer.setRGB(i, red, green, blue);
+        }
+        led.setData(ledbuffer);
+        led.start();
+    }
+}
